4466dfd64d4c11b43f0443a5823e44581bd246db
kapil-0455 <kapil0455cu@gmail.com>
2026-05-07 23:02:41 +0530

    implement connection requests component , accept or reject

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { addRequests } from '../utils/requestSlice'
+import { addRequests, removeRequest } from '../utils/requestSlice'
 import axios from 'axios'
 import { BASE_URL } from '../utils/constants'
 
@@ -8,6 +8,15 @@ const Requests = () => {
 
     const dispatch = useDispatch();
     const requests = useSelector((state) => state.requests)
+
+    const reviewRequest = async(status , _id)=>{
+        try {
+            const res = await axios.post(BASE_URL + "/request/review" + '/'+ status + '/' + _id, {} , {withCredentials : true});
+            dispatch(removeRequest(_id));
+        } catch (err) {
+            console.error("Error Reviewing Request", err)
+        }
+    }
     const fetchRequests = async()=>{
         try {
             const res = await axios.get(BASE_URL + '/user/request/recived' , {
@@ -19,6 +28,7 @@ const Requests = () => {
         }
         
     }
+
     useEffect(() => {
         fetchRequests(); 
     }, []);
@@ -43,6 +53,20 @@ const Requests = () => {
                 <h2 className='font-bold text-2xl'>{firstName + " " + lastName}</h2>
                {age && gender && <p className="text-gray-400">{age + ", " + gender}</p> } 
                 <p className="mt-2">{description}</p>
+              </div>
+
+              <div className="flex flex-col sm:flex-row gap-3 ml-4">
+
+                <button className="btn btn-primary btn-sm md:btn-md hover:scale-105 transition-transform" 
+                onClick={()=>reviewRequest("accepted", request._id)}>
+                    Accept
+                </button>
+
+                <button className="btn btn-outline btn-secondary btn-sm md:btn-md hover:scale-105 transition-transform" 
+                onClick={()=>reviewRequest("rejected", request._id)}>
+                    Decline
+                </button>
+
               </div>
             </div>
         );
